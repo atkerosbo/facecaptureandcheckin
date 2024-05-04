514432531d354635a268fe6bb0ec39967cfcc430
atkerosbo <atkerosbo@yahoo.com>
2024-05-04 11:47:02 +0200

    Initial push

--- main.py
+++ main.py
@@ -5,19 +5,23 @@ import models
 
 models.Base.metadata.create_all(engine)
 
-
+#Function to add new face
 def add_face():
     user_name = input("Please enter your name: ")
     new_face = FaceModel(name=user_name, encoding=face_encoding())
     session.add(new_face)
     session.commit()
-    return (f"new Face was added with username : {user_name}")
-
+    return print(f"new Face was added with username : {user_name}")
+#Dunction to compare faces
 def compare_faces():
     
     face_to_compare = input("Please enter the name of the face to compare: ")
     face_to_compare_encoding = session.query(FaceModel).filter_by(name=face_to_compare).first().encoding
-    user_face_encoding = face_encoding()
+    if face_to_compare_encoding == None:
+        print("Face to compare not found")
+    else:
+        user_face_encoding = face_encoding()
+        print(f"Encoding the face for {face_to_compare}")
 
 
     if user_face_encoding == face_to_compare_encoding:
@@ -28,9 +32,9 @@ def compare_faces():
         else:
             print("Checkin was canceled")
     else:
-        print(f"The binary data wasnt a match !")
+        print(f"The binary data wasn't a match !")
     
-
+#Function to check in if the binary data is a match
 def check_in():
     
     new_check_in_user_name = input("Please enter your name: ")
@@ -39,7 +43,7 @@ def check_in():
     session.commit()
     return (f"new CheckIn was crated with usernames face binary data : {new_check_in_user_name}")
 
-
+#operations
 operation = input("Press C to compare faces, N to add new:")
 if operation.capitalize() == "C":
     compare_faces()
