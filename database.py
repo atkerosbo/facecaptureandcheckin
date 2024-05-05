c8a2581a3497268675158f76164fcd4b070b729b
atkerosbo <atkerosbo@yahoo.com>
2024-05-05 16:02:40 +0200

    req + db fix

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
-from settings import Settings
+from settings import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Define the PostgreSQL database URL
-db_url = f"postgresql://{Settings.database_username}:{Settings.database_password}@{Settings.database_hostname}:{Settings.database_port}/{Settings.database_name}"
+db_url = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 # Create an engine and session
 engine = create_engine(db_url)
